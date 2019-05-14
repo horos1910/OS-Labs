4279639b44820d4a70636e6b3c3a266ae300607b
horos1910 <48953771+horos1910@users.noreply.github.com>
2019-05-14 09:13:40 +0300

    Update suzen-lvl14.sh

--- Lab-work3/suzen-lvl14.sh
+++ Lab-work3/suzen-lvl14.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-echo "Flag is: $(ls part1/)$(ls ../john/Documents/part2/)$(ls ../john/Desktop/part3/)"
+cd part1
+ls
+cd ../..
+cd john/Documents/part2
+ls
+cd ../..
+cd Desktop/part3
+ls
+echo Flag is: d2FodmFoMWFlV2FpYmVlaG9vMmIK
