25cd6f4fbce6d6ff16f5280f70d6abd984df4848
horos1910 <48953771+horos1910@users.noreply.github.com>
2019-05-14 09:20:12 +0300

    Update suzen-lvl19.sh

--- Lab-work3/suzen-lvl19.sh
+++ Lab-work3/suzen-lvl19.sh
@@ -1,1 +1,6 @@
-
+#!/bin/bash
+mkdir -p Documents/projects/lab19/using/simple/bash/commands
+cd Documents/projects/lab19/using/simple/bash/commands
+sleep 60
+ls -a
+echo Flag is: V2VpMGNvaHNoZWlxdWE0YWhnaG8K
